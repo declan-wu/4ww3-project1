ff580cf7106176771835d9e3e8a9966ca7c409c3
Jennifer Cheng <jennifer.zhen.cheng@gmail.com>
2021-12-08 16:50:32 -0500

    update company name lol

--- includes/footer.php
+++ includes/footer.php
@@ -18,7 +18,7 @@
         </ul>
       </div>
       <div class="col-md-6 item text">
-        <h3>Company Name</h3>
+        <h3>Restaurant Finder</h3>
         <p>4WW3 Project, Fall 2021. Group members: Jennifer Cheng, Song Tao (Declan) Wu.</p>
       </div>
 
